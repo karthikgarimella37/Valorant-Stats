0f51b1b0982dfd6069731c39df405496cc6c7ffd
Karthik Garimella <karthikgarimella37@gmail.com>
2026-08-29 19:18:41 -0500

    chore: auto-commit after file change

--- src/backend/vlr/__init__.py
+++ src/backend/vlr/__init__.py
@@ -1,10 +1,12 @@
 from .extract import VlrExtractPipeline, landing_dir_for
 from .probe_api_coverage import run_probe
+from .scrape_economy import attach_round_economy
 from .snapshot_json import write_entity_json
 from .watermarks import load_watermarks, upsert_watermark
 
 __all__ = [
     "VlrExtractPipeline",
+    "attach_round_economy",
     "landing_dir_for",
     "load_watermarks",
     "run_probe",
