c8dae7aa742d909b1d118a3c12a2b881099f9137
Karthik Garimella <karthikgarimella37@gmail.com>
2026-09-15 23:11:22 -0500

    feat: update __init__.py
    M  src/backend/vlr/fact/__init__.py

--- src/backend/vlr/fact/__init__.py
+++ src/backend/vlr/fact/__init__.py
@@ -1,8 +1,5 @@
 from .pipeline import apply_facts_schema, extract_facts, load_facts, run_facts
-from .util import fact_json_dir
-
-# Back-compat alias used by older fact/__init__.
-from .util import facts_dir as fact_json_dir  # noqa: F811
+from .util import facts_dir as fact_json_dir
 
 __all__ = [
     "apply_facts_schema",
